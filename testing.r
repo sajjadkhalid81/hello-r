1ab6c6d036feadf11cf996d2634d23de2a3c93aa
sajjadkhalid81 <sajjad.khalid@hotmail.com>
2021-12-19 20:39:42 +0500

    line added

--- testing.r
+++ testing.r
@@ -1,3 +1,10 @@
 # This is from Git
 print("Hello from Github")
+print("Hello from sajjad")
+
+name = "sajjad"
+version = "Rstudio in writting"
+
+print(name)
+print("these lines has been written in Rstudio")
 
